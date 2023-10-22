c22d7bac074f8905840d3b43bc1bea3856c088ca
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-10-22 20:20:08 -0300

    feat: ensure MongoGetUsersRepository gets users from mongo

--- src/repositories/getUsers/mongoGetUsers.ts
+++ src/repositories/getUsers/mongoGetUsers.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/getUsers/protocols";
 import { User } from "../../models/user";
+import { MongoClient } from "./../../database/mongo";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "AAAA",
-        lastName: "BBBB",
-        email: "CCCC",
-        password: "DDDD",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
