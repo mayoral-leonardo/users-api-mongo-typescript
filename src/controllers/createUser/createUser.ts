2b7a3477c6c92b23c9169433b0ec5f42489cb3b4
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-10-23 21:26:49 -0300

    feat: add email validator

--- src/controllers/createUser/createUser.ts
+++ src/controllers/createUser/createUser.ts
@@ -1,3 +1,4 @@
+import validator from "validator";
 import { User } from "../../models/user";
 import { HttpRequest, HttpResponse } from "../protocols";
 import {
@@ -27,6 +28,15 @@ export class CreateUserController implements ICreateUserController {
             body: `Field ${field} is required`,
           };
         }
+      }
+
+      //Verificar se o e-mail é válido
+      const isEmailValid = validator.isEmail(httpRequest.body!.email);
+      if (!isEmailValid) {
+        return {
+          statusCode: 400,
+          body: "E-mail is invalid",
+        };
       }
 
       const user = await this.createUserRepository.createUser(
