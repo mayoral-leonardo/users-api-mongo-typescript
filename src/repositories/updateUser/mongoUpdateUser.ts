b14336a353adb27b00e83ec9322b8ddd13a27334
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-11-02 15:27:33 -0300

    feat: add MongoUpdateUserRepository

---
+++ src/repositories/updateUser/mongoUpdateUser.ts
@@ -0,0 +1,32 @@
+import { ObjectId } from "mongodb";
+import {
+  IUpdateUserRepository,
+  UpdateUserParams,
+} from "../../controllers/updateUser/protocols";
+import { User } from "../../models/user";
+import { MongoClient } from "./../../database/mongo";
+
+export class MongoUpdateUserRepository implements IUpdateUserRepository {
+  async updateUser(id: string, params: UpdateUserParams): Promise<User> {
+    await MongoClient.db.collection("users").updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $set: {
+          ...params,
+        },
+      }
+    );
+
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!user) {
+      throw new Error("User not updated");
+    }
+
+    const { _id, ...rest } = user;
+
+    return { id: _id.toHexString(), ...rest };
+  }
+}
